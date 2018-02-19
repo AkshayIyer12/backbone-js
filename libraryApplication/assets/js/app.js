c509d4d08242b6094383a2ccf50eab7eb7044242
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-02-19 22:42:21 +0530

    Add hello world with templates

--- libraryApplication/assets/js/app.js
+++ libraryApplication/assets/js/app.js
@@ -1,11 +1,12 @@
 let App = Backbone.View.extend({
-  el: '#app',
+  el: $('#app'),
+  template: _.template('<h3>Hello <%= who %></h3>'),
   initialize () {
     console.log('Initialized')
     this.render()
   },
   render () {
-    this.$el.html('Hello World')
+    this.$el.html(this.template({who: 'world!'}))
   }
 })
 let app = new App()
