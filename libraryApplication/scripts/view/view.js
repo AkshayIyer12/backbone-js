f87c4eb1ed6908c9a1bfc9c625707797a9338fe0
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-02-26 15:30:05 +0530

    Add jqgrid support

--- libraryApplication/scripts/view/view.js
+++ libraryApplication/scripts/view/view.js
@@ -5,6 +5,7 @@ define(function (require) {
   const uuidv4 = require('uuid-v4')
   const _ = require('underscore')
   const dust = require('dust')
+  const jqGrid = require('jqGrid')
   const buttonsTemplate = require('text!templates/buttons.dust')
   const modalTemplate = require('text!templates/modal.dust')
   const tableTemplate = require('text!templates/table.dust')
@@ -24,6 +25,7 @@ define(function (require) {
       'click #selectall': 'selectAllCheckbox',
       'click .addEdit': 'resetForm',
       'click #edit': 'showModal',
+      'click #add': 'addBookModal',
       'click #editBook': 'updateBook'
     },
     initialize (Collection, Model) {
@@ -37,14 +39,14 @@ define(function (require) {
       let self = this
       let reducer = _.reduce(arr, function (accum , v) {
         let obj = {
-          title: v[0],
-          author: v[1],
+          Title: v[0],
+          Author: v[1],
           id: v[2]
         }
         accum.push(obj)
         return accum
       }, [])
-      _.map(reducer, v => this.createAndDisplayModel(v))
+     _.map(reducer, v => this.createAndDisplayModel(v))
     },
     render () {
       let self = this.$el
@@ -58,7 +60,16 @@ define(function (require) {
       })
       dust.renderSource(tableTemplate, {}, function (err, res) {
         if (err) console.error(err)
-        self.find('#display').append(res)
+        self.find('#display2').jqGrid({
+          colModel: [
+            {name: 'Title', width: 160},
+            {name: 'Author', width: 160}
+          ],
+          data: [],
+          multiselect: true,
+          guiStyle: 'bootstrap',
+          iconSet: 'fontAwesome'
+        })
       })
     },
     addBook () {
@@ -66,8 +77,8 @@ define(function (require) {
       this.title = this.$el.find('#title')[0]
       this.author = this.$el.find('#author')[0]
       let obj = {
-        title: this.title.value,
-        author: this.author.value,
+        Title: this.title.value,
+        Author: this.author.value,
         id: this.id
       }
       this.createAndDisplayModel(obj)
@@ -77,9 +88,7 @@ define(function (require) {
       let libStoreObj = new this.LibraryStore(obj)
       this.libList.add(libStoreObj)
       let self = this.$el
-      dust.renderSource(rowTemplate, {'book': [obj]}, (err, res) => {
-        self.find('tbody').append(res)
-      })
+      self.find('#display2').jqGrid('addRowData', obj.id, obj)
     },
     deleteBook () {
       let self = this
@@ -91,21 +100,19 @@ define(function (require) {
         }
       })
     },
-    selectAllCheckbox () {
+    addBookModal () {
       let self = this.$el
-      self.find('#selectall').change(function () {
-        let val = this.checked
-        self.find('input:checkbox').prop('checked', val)
-      })
+      self.find('#title')[0].value = ''
+      self.find('#author')[0].value = ''
     },
     showModal () {
       let self = this.$el
-      let checkedItem = self.find('.tableCheck:checked')
       let popUp = self.find('#popup')[0]
-      let length = checkedItem.length
+      let val = self.find('#display2').jqGrid('getGridParam', 'selarrrow')
+      let length = val === null ? 0 : val.length
       this.modalAttribute(length)
       if (length > 1) this.showCheckBoxError(popUp, true)
-      else (checkedItem.length === 1) ? this.fillFormData(checkedItem[0].parentElement.parentElement) : this.showCheckBoxError(popUp, false)
+      else (length === 1) ? this.fillFormData(val) : this.showCheckBoxError(popUp, false)
     },
     showCheckBoxError (popUp, length) {
       dust.renderSource(warningTemplate, {"len": length}, (err, res) => {
@@ -120,44 +127,36 @@ define(function (require) {
       let modalSet = this.$el.find('#edit')[0]
       return (length === 1) ? modalSet.setAttribute('data-target', '#myModal') : modalSet.setAttribute('data-target', '')
     },
-    fillFormData ({id}) {
+    fillFormData ([id]) {
       let self = this.$el
       self.find('#edit')[0].setAttribute('data-target', '#myModal')
       self.find('#input')[0].parentElement.setAttribute('class', id)
-      let {title, author} = this.libList.get(id).attributes
+      let {Title, Author} = this.libList.get(id).attributes
       let obj = {
-        '#title': title,
-        '#author': author
+        '#title': Title,
+        '#author': Author
       }
       _.each(_.keys(obj), a => self.find(a)[0].setAttribute('value', obj[a]))
     },
     resetForm (e) {
       let {id} = e.currentTarget
       let self = this.$el
-      let node = self.find('#input')[0].children
+      let node = self.find('.modal-footer')[0].children[0]
       self.find('form').trigger('reset')
-      let arr = _.filter(_.toArray(node), (v, i) => i === 2 ? v : null)
-      _.each(arr, a => {
-        a.setAttribute('id', `${id}Book`)
-        a.innerHTML = `${capitalizeHead(id)} Book`
-        return a
-      })
+      node.setAttribute('id', `${id}Book`)
+      node.innerHTML = `${capitalizeHead(id)} Book`
     },
     updateBook () {
       let self = this.$el
-      let [title, author] = [this.title.value, this.author.value]
+      let [title, author] = [self.find('#title')[0].value, self.find('#author')[0].value]
       let id = self.find('#input')[0].parentElement.className
       let model = this.libList.get(id)
-      model.set({title: title, author: author})
+      model.set({Title: title, Author: author})
       this.libList.set({model}, {remove: false})
-      let arr = _.toArray(self.find(`#${id}`)[0].children)
-      let num = _.filter(arr, (a, i) => i !== 0 ? a : null)
-      _.each(num, (a, i) => {
-        let val = (i === 0) ? title : author
-        a.innerHTML = val
-        return a
-      })
-      self.find('#title')[0].removeAttribute('value')
+      let localRowData = self.find('#display2').jqGrid('getLocalRow', id)
+      localRowData['Title'] = title
+      localRowData['Author'] = author
+      self.find('#display2').jqGrid('setRowData', id, localRowData)
     }
   })
   return App
